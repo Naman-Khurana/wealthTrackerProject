62bcb4f0178ed3d6996679768e3dd2dfef996244
Naman Khurana <namankhurana2017@gmail.com>
2026-02-18 19:15:58 +0530

    implemented comman card in statcard block

--- wealth-tracker-frontend/src/components/comman/ui/money_stat_card.tsx
+++ wealth-tracker-frontend/src/components/comman/ui/money_stat_card.tsx
@@ -1,26 +1,29 @@
 import { BACKGROUND_COLOR_EMRALD_GREEN, TEXT_COLOR_EMRALD_GREEN } from "@/constants/ui.constants";
-import { title } from "process";
 
-type MoneyStatCardProp={
-    title : string;
+import Card from "./card";
+
+type MoneyStatCardProp = {
+    title: string;
     amount: number
 }
 
 export default function MoneyStatCard({
-    title ,
+    title,
     amount
 }: MoneyStatCardProp
-){
-    return(
-            <div key={title} className="h-[90%] w-[50%] bg-gray-500/20 rounded-2xl   flex flex-col justify-center 
-            pl-[5%]   shadow-lg shadow-black/40">
-                <div className="text-white/70 text-[0.9rem] font-semibold  ">{title}</div>
-                <div className="text-[1.7rem] flex flex-row gap-1 ">
-                    <div className={` ${TEXT_COLOR_EMRALD_GREEN}`}>₹</div>
-                    <div>{amount}</div>
-                     
-                </div>
+) {
+    return (
+        <Card className="h-[90%] w-[50%] flex flex-col justify-center" >
+
+
+            <div className="text-white/70 text-[0.9rem] font-semibold  ">{title}</div>
+            <div className="text-[1.7rem] flex flex-row gap-1 ">
+                <div className={` ${TEXT_COLOR_EMRALD_GREEN}`}>₹</div>
+                <div>{amount}</div>
 
             </div>
-        )
+
+
+        </Card>
+    )
 }
